09b1e569018a2901f9302d3565c130999c660b81
Matthias Günther <matthias.guenther@wikimatze.de>
2010-05-24 09:19:05 +0200

    reworked 'if/else und while'; removed unused files

---
+++ beispiele/if_else_ternary_while.scala
@@ -0,0 +1,11 @@
+if (exp) println("yes")
+
+// ternary + if else
+val i: Int = if (exp) 1 else 2
+
+// while
+while (exp) println("Working...")
+while (exp) {
+  println("Working...")
+}
+
---
+++ executable/Generator.scala
@@ -0,0 +1,13 @@
+object Generator extends Application{
+  val clans = List("Eshin", "Test", 2)
+  
+  // one not so real generator
+  for(i <- clans
+    if clans.contains("Eshin")
+  ) println("Skaven!")
+  
+  // a real generator
+  for(j <- clans
+    if j == "Eshin"
+  ) println("Skaven are here")
+}
