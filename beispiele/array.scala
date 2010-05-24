220ce05d22d601430d64e3cdab2cb17a0b5404c1
Matthias Günther <matthias.guenther@wikimatze.de>
2010-05-24 08:53:38 +0200

    reworked 'Operationen sind Objekte' and commented 'array.scala'

--- beispiele/array.scala
+++ beispiele/array.scala
@@ -1,13 +1,16 @@
+// exist an element in xs with property p?
 def exists[T](xs: Array[T], p: T => boolean) = {
   var i: int = 0
   while (i < xs.length && !p(xs(i))) i = i +1
   i < xs.length
 }
 
-def forall[T](xs: Array[T], p: T => boolean) = { # nested functions
+def forall[T](xs: Array[T], p: T => boolean) = {
+  // nested function
   def not_p(x: T) = !p(x)
   !exists(xs, not_p)
 }
 
+// shorter and eleganter than forAll (works with anonymous function)
 def forallAnonymous[T](xs: Array[T], p: T => boolean) =
   !exists(xs, (x: T) => !p(x))
