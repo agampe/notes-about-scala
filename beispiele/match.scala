dd54edc899c29e6f7cd27deeff2b2fb4aaab8609
Matthias Günther <matthias.guenther@wikimatze.de>
2010-05-24 16:02:38 +0200

    reworked 'Pattern-Matching'

--- beispiele/match.scala
+++ beispiele/match.scala
@@ -4,25 +4,25 @@
 }
 
 
-# pattern-Match fuer Klassen
+// pattern-Match fuer Klassen
 Stuff("David", 45) match {
   case Stuff("David", 45) => true
   case _ => false
 }
 
-# koennen den Namen testen, wobei uns der zweite Parameter (age) rille ist
+// koennen den Namen testen, wobei uns der zweite Parameter (age) rille ist
 Stuff("David", 45) match {
   case Stuff("David", _) => "David"
   case _ => "Other"
 }
 
-# koennen das age field extrahieren und in die howold-Variable schreiben
+// koennen das age field extrahieren und in die howold-Variable schreiben
 Stuff("David", 45) match {
   case Stuff("David", howOld) => "David, age: "+howOld
   case _ => "Other"
 }
 
-# koennen einen Guard setzen
+// koennen einen Guard setzen
 Stuff("David", 45) match {
   case Stuff("David", age) if age < 30 => "young David"
   case Stuff("David", _) => "old David"
