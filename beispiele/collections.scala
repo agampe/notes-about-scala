b574e99ea22f62eab495de4751d2ff42102a1dd8
Matthias Günther <matthias.guenther@wikimatze.de>
2010-05-24 16:46:49 +0200

    removed 'Scala Listen, Tupel und Map-Klassen'

--- beispiele/collections.scala
+++
@@ -1,7 +0,0 @@
-val x = List(1,2,3,4)
-x.filter(a => a % 2 == 0) // List[Int] = List(2, 4)
-
-val a = Array(1,2,3) // Arrays fangen bei null an
-a(1) // 2
-
-val m = Map("one" -> 1, "two" -> 2, "three" -> 3)
--- beispiele/range.scala
+++
@@ -1,2 +0,0 @@
-0 to 10
-// Range.Inclusive = Range(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
