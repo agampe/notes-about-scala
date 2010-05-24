145d438cf47060a76dc00538756a3d2ee3141f95
Matthias Günther <matthias.guenther@wikimatze.de>
2010-05-24 16:25:12 +0200

    removed 'Annotationen und Varianz'

--- beispiele/stack_generisch.scala
+++ beispiele/stack_generisch.scala
@@ -4,11 +4,13 @@ abstract class Stack[A] {
   def top: A
   def pop: Stack[A]
 }
+
 class EmptyStack[A] extends Stack[A] {
   def isEmpty = true
   def top = error("EmptyStack.top")
   def pop = error("EmptyStack.pop")
 }
+
 class NonEmptyStack[A](elem: A, rest: Stack[A]) extends Stack[A] {
   def isEmpty = false
   def top = elem
@@ -17,3 +19,4 @@ class NonEmptyStack[A](elem: A, rest: Stack[A]) extends Stack[A] {
 
 val x = new EmptyStack[Int]
 val y = x.push(1).push(2)
+
--- beispiele/stack_integer.scala
+++ beispiele/stack_integer.scala
@@ -4,13 +4,16 @@ abstract class IntStack {
   def top: Int
   def pop: IntStack
 }
+
 class IntEmptyStack extends IntStack {
   def isEmpty = true
   def top = error("EmptyStack.top")
   def pop = error("EmptyStack.pop")
 }
+
 class IntNonEmptyStack(elem: Int, rest: IntStack) extends IntStack {
   def isEmpty = false
   def top = elem
   def pop = rest
 }
+
