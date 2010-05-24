4bd7538f9d2780ad583c4f4495a98a9e68f0d771
Matthias Günther <matthias.guenther@wikimatze.de>
2010-05-24 16:58:09 +0200

    reworked 'List[T]'

--- beispiele/cons.scala
+++ beispiele/cons.scala
@@ -1,5 +1,5 @@
-1 :: 2 :: 3 :: Nil
 // List[Int] = List(1, 2, 3)
+1 :: 2 :: 3 :: Nil
 
-// obiger Code wird auf folgende weisse ausgewertet
+// obiger Code wird auf folgenderweisse ausgewertet
 new ::(1, new ::(2, new ::(3, Nil)))
--- beispiele/list_arbitrary.scala
+++
@@ -1,2 +0,0 @@
-List(1, 44.5, 8d)
-List[AnyVal] = List(1, 44.5, 8.0)
