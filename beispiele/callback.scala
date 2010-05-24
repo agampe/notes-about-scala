7a4b8b80280f1fbbbbd6c61b4cae682df427e108
Matthias Günther <matthias.guenther@wikimatze.de>
2010-05-24 12:45:39 +0200

    reworked 'Callback'

--- beispiele/callback.scala
+++ beispiele/callback.scala
@@ -1,11 +1,11 @@
-object Timer {
-  def oncePerSecond(callback: () => Unit) {
-    while (true) { callback(); Thread sleep 1000 }
-  }
-  def timeFlies() {
-    println("time flies like an arrow...")
-  }
-  def main(args: Array[String]) {
-    oncePerSecond(timeFlies)
-  }
+def oncePerSecond(callback: () => Unit) {
+  while (true) { callback(); Thread sleep 10 }
 }
+
+def timeFlies() {
+  println("The dwarfs will pass away ...")
+}
+
+
+oncePerSecond(timeFlies) // start for 10 seconds the thread
+
