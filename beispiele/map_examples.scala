3e1b7196150a518b52355611033bb075e267b2f2
Matthias Günther <matthias.guenther@wikimatze.de>
2010-05-28 08:16:33 +0200

    reworked 'Map[K,V]'

--- beispiele/map_examples.scala
+++ beispiele/map_examples.scala
@@ -1,12 +1,11 @@
-var p = Map(1 -> "David", 9 -> "Elwood")
-// Map[Int,java.lang.String] = Map(1 -> David, 9 -> Elwood)
+var p = Map(1 -> "ruby", 9 -> "scala")
+// Map[Int,java.lang.String] = Map(1 -> ruby, 9 -> scala)
 
 // an eine Map noch ein Element dranhaengen
 p + 2 -> "test"
-// Map[Int,java.lang.String] = Map(1 -> David, 9 -> Elwood, 2 -> test)
 
-p(1)          // funzt
-p(88)         // exception
+p(1)          // jo
+p(88)         // eek
 p.get(88)     // Option[java.lang.String] = None
 p -= 9        // key-value entfernen
 p.contains(1) // true
--- beispiele/tupel_example.scala
+++ beispiele/tupel_example.scala
@@ -1,7 +1,7 @@
 def sumSq(in: List[Double]): (Int, Double, Double) =
     in.foldLeft((0, 0d, 0d))((t, v) => (t._1 + 1, t._2 + v, t._3 + v * v))
 
-// koennen obige Funktion durch Scalas pattern-Matching lesbarer machen
+// obiges pattern-Matching lesbarer
 
 def sumSqReadable(in: List[Double]) : (Int, Double, Double) =
     in.foldLeft((0, 0d, 0d)){
