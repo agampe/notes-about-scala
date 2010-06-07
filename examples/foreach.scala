ebddb99c9f95276577853daec7c8065cbdd07192
Matthias Günther <matthias.guenther@wikimatze.de>
2010-06-07 18:12:33 +0200

    add example foreach

--- examples/foreach.scala
+++ examples/foreach.scala
@@ -1,13 +1,14 @@
-List(1, 2, 3, 4, 5) foreach { i => println("Int: " + i) }
+var my_list = List("Test1", "Test2", "Test3", 1)
+my_list.foreach(elem => println(elem))
+//  my_list.foreach((elem: Int) => println(elem)) funzt nicht
+my_list.foreach((elem: Any) => println(elem)) // funzt
+
+List(1, 2, 3, 4, 5) foreach { i => println("Int: " + i) }  
+
+
 val stateCapitals = Map(
   "Lustria" -> "Montgomery",
   "Great Mountains" -> "Karaz-A-Karak",
   "Imperium" -> "Altdorf")
 stateCapitals foreach { kv => println(kv._1 + ": " + kv._2) }
-
-trait Iterable[+A] {
-  ...
-  def foreach(f : (A) => Unit) : Unit = ...
-  ...
-}
 
