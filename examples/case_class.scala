d0298aa25c4ed7f026061bdb46e3b578190c7112
Matthias Günther <matthias.guenther@wikimatze.de>
2010-06-09 11:16:33 +0200

    reworked pattern-matching and case-classes

--- examples/case_class.scala
+++ examples/case_class.scala
@@ -1,15 +1,16 @@
 case class Stuff(name: String, age: Int)
 
-# erzeuge eine Instanz
+// eine Instanz erzeugen
 val s = Stuff("Arsch", 24)
 
-# equals-Methode anwenden
+// equals-Methode anwenden
 s == Stuff("Arsch", 24)
 
-# Zugriff auf die Member-Variablen
+// auf Members zugreifen
 s.name; s.age;
 
-# eigene Klasse schreiben, die das gleiche leistet wie die case-Klasse
+// eigene Klasse schreiben, die das gleiche leistet wie die case-Klasse
+// hat viel mehr ekligen Code
 class Stuff(val name: String, val age: Int) {
   override def toString = "Stuff("+name+","+age+")"
   override def hashCode = name.hashCode + age
--- examples/pattern_matching_fibonacci.scala
+++ examples/pattern_matching_fibonacci.scala
@@ -9,5 +9,5 @@ def fibonacci(input: Int): Int = input match {
 def fib2(in: Int): Int = in match {
   case n if n <= 0 => 0
   case 1 => 1
-  case n => fib2(n - 1) + fib2(n - 2)
+  case _ => fib2(n - 1) + fib2(n - 2)
 }
