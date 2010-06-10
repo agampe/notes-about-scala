665384224da7b94b7dd5f82a522ab40ed4d450d8
Matthias Günther <matthias.guenther@wikimatze.de>
2010-06-10 07:02:12 +0200

    new chapter 'Packages' with example

---
+++ examples/packages.scala
@@ -0,0 +1,14 @@
+// all class which are defined in com.wikiMatze
+package com.wikiMatze;
+
+// import everything of java.util
+import java.util._;
+
+// import only one Class
+import com.wikiMatze.SoUndSo;
+
+// import SoUndSo as Test
+import com.wikiMatze.{SoUndSo => Test}
+
+// import only three classes
+import com.wikiMatze{Bla, Blu, Bli}
